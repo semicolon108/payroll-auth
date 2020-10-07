a625cd3dd1f2615a740c741819b8b36a748a1748
phongvilai noypha <phongvilaitoto@gmail.com>
2020-10-07 11:26:27 +0700

    chane url

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const isProd = process.env.NODE_ENV === 'production' ? true : false
 
-const baseURL = isProd ? 'https://payroll.laofix.com/graphql' : 'http://localhost:4000'
+const baseURL = isProd ? 'https://payroll.sabai.la/graphql' : 'http://localhost:4000/graphql'
 
 const apolloClient = new ApolloClient({
     uri: baseURL,
