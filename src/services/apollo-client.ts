01ca8f6377098857d7d37a987adaa420be252521
phongvilai noypha <phongvilaitoto@gmail.com>
2021-01-07 14:26:29 +0700

    change domain

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production' && true
 
 
 export const loginURL = isProd ? 'https://app-payroll.sabai.la' : 'http://localhost:8080'
-const baseURL = isProd ? 'https://payroll.sabai.la/graphql' : 'http://localhost:4000/graphql'
+const baseURL = isProd ? 'https://hr-sabai.la/graphql' : 'http://localhost:4000/graphql'
 
 const apolloClient = new ApolloClient({
     uri: baseURL,
