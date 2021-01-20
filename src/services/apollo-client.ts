856c69329da5f38beceb6a7eaf38d9f27a5031c6
phongvilai noypha <phongvilaitoto@gmail.com>
2021-01-20 13:19:28 +0700

    change url

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const isProd = process.env.NODE_ENV === 'production' && true
 
 
-export const loginURL = isProd ? 'https://app-payroll.sabai.la' : 'http://localhost:8080'
+export const loginURL = isProd ? 'https://app.hr-sabai.com' : 'http://localhost:8080'
 const baseURL = isProd ? 'https://hr-sabai.com/graphql' : 'http://localhost:4000/graphql'
 
 const apolloClient = new ApolloClient({
