d0f7e0da0c21e267bbab9575671dfa684765c111
phongvilaitoto <phongvilaitoto@gmail.com>
2025-02-21 17:08:17 +0700

    build2

---
+++ build/precache-manifest.f933ba70efe965509730bfab63be245c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e056b20f23b8dd501ae012866d9d217b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b9015c9eedef46f8a5d",
+    "url": "/static/css/main.b8f04afd.chunk.css"
+  },
+  {
+    "revision": "76b4595eaeb41af25fca",
+    "url": "/static/js/2.66b8219b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.66b8219b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b9015c9eedef46f8a5d",
+    "url": "/static/js/main.dcb8518c.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  }
+]);
