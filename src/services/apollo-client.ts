cccc5309779b95cf6b0c50069550bfdb231c527a
phongvilaitoto <phongvilaitoto@gmail.com>
2025-02-21 13:50:47 +0700

    ud base url

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -3,8 +3,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const isProd = process.env.NODE_ENV === 'production' && true
 
 
-export const loginURL = isProd ? 'https://app.hr-sabai.com' : 'http://localhost:8080'
-const baseURL = isProd ? 'https://hr-sabai.com/graphql' : 'http://localhost:4000/graphql'
+export const loginURL = isProd ? 'https://payroll.hr-sabai.com' : 'http://localhost:8080'
+const baseURL = isProd ? 'https://payroll-server.hr-sabai.com/graphql' : 'http://localhost:4000/graphql'
 
 
 const apolloClient = new ApolloClient({
